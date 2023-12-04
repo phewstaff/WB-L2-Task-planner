729c3a6e7416a587d39483771c8a72509584160a
magomed_n <naurbaev2020@gmail.com>
2023-12-04 21:40:20 +0300

    fix: Пофиксил баги, изменяющие текст при фокусе

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,35 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
 
 const TodoItem = ({ item, focusedId, setFocusedId, todo, setTodo }) => {
+  const [isActive, setIsActive] = useState(false);
   const { updateTodo } = useContext(AppContext);
 
   const handleOnBlur = () => {
     updateTodo(todo);
+    setIsActive(false);
   };
 
   const handleOnChange = (e) => {
     setTodo({ id: item.id, text: e.target.value });
   };
 
-  const handleOnClick = () => {
+  const handleOnClick = (e) => {
+    setIsActive(true);
+    setFocusedId(item.id);
+  };
+
+  const handleOnFocus = () => {
+    setIsActive(true);
     setTodo({ text: item.text });
     setFocusedId(item.id);
   };
 
   return (
-    <div key={item.id}>
-      <p>{item.id}</p>
-      <input
-        onClick={handleOnClick}
-        autoFocus
-        id={item.id}
-        value={item.id === focusedId ? todo.text : item.text}
-        onChange={handleOnChange}
-        onBlurCapture={handleOnBlur}
-        type="text"
-      />
+    <div className="todo-container" key={item.id}>
+      <input type="radio" />
+
+      <div className="todo-item-content">
+        <input
+          onFocus={handleOnFocus}
+          className="todo-input"
+          // onClick={handleOnClick}
+          autoFocus
+          id={item.id}
+          value={item.id === focusedId ? todo.text : item.text}
+          onChange={handleOnChange}
+          onBlurCapture={handleOnBlur}
+          type="text"
+        />
+      </div>
+
+      {isActive && <h3 onClick={handleOnClick}>Add note</h3>}
+      <hr />
     </div>
   );
 };
